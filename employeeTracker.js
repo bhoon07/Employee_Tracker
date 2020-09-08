8df4fbef9e41ab8a6405b8b8f28e07dadd5dd108
Blaine Hoon <bhoon07@gmail.com>
2020-09-08 16:00:24 -0500

    updates

---
+++ employeeTracker.js
@@ -0,0 +1,118 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  port: 3306,
+
+  user: "root",
+
+  password: "smartcar",
+  database: "employeeTrackerdb"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runSearch();
+});
+
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "Select a Department",
+      choices: [
+        "Financial",
+        "Legal",
+        "Engineering",
+        "Sales"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "Select an employee":
+        Search();
+        break;
+      }
+    });
+}
+
+function Search() {
+  inquirer
+    .prompt({
+      name: "",
+      type: "input",
+      message: "What artist would you like to search for?"
+    })
+    .then(function(answer) {
+      var query = "SELECT position, song, year FROM top5000 WHERE ?";
+      connection.query(query, { artist: answer.artist }, function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+          console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+        }
+        runSearch();
+      });
+    });
+}
+
+function songSearch() {
+  inquirer
+    .prompt({
+      name: "song",
+      type: "input",
+      message: "What song would you like to look for?"
+    })
+    .then(function(answer) {
+      console.log(answer.song);
+      connection.query("SELECT * FROM top5000 WHERE ?", { song: answer.song }, function(err, res) {
+        console.log(
+          "Position: " +
+            res[0].position +
+            " || Song: " +
+            res[0].song +
+            " || Artist: " +
+            res[0].artist +
+            " || Year: " +
+            res[0].year
+        );
+        runSearch();
+      });
+    });
+}
+
+function songAndAlbumSearch() {
+  inquirer
+    .prompt({
+      name: "artist",
+      type: "input",
+      message: "What artist would you like to search for?"
+    })
+    .then(function(answer) {
+      var query = "SELECT top_albums.year, top_albums.album, top_albums.position, top5000.song, top5000.artist ";
+      query += "FROM top_albums INNER JOIN top5000 ON (top_albums.artist = top5000.artist AND top_albums.year ";
+      query += "= top5000.year) WHERE (top_albums.artist = ? AND top5000.artist = ?) ORDER BY top_albums.year, top_albums.position";
+
+      connection.query(query, [answer.artist, answer.artist], function(err, res) {
+        console.log(res.length + " matches found!");
+        for (var i = 0; i < res.length; i++) {
+          console.log(
+            i+1 + ".) " +
+              "Year: " +
+              res[i].year +
+              " Album Position: " +
+              res[i].position +
+              " || Artist: " +
+              res[i].artist +
+              " || Song: " +
+              res[i].song +
+              " || Album: " +
+              res[i].album
+          );
+        }
+
+        runSearch();
+      });
+    });
+}
